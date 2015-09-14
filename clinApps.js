e6b214a3f20493749577a949dca8f84b5e41e9b3
Jonas Almeida <jalmeida@mathbiol.org>
2015-09-14 11:46:49 -0400

    check arrow update

--- clinApps.js
+++ clinApps.js
@@ -7,7 +7,7 @@ clinApps= function(){
 
 clinApps.setMsg=function(){
     if(typeof(clinAppsMsg)==="object"){
-        clinAppsMsg.innerHTML=' &nbsp; &nbsp; &nbsp; &nbsp; <input type="radio"> Operations &nbsp; <input type="radio"> Research &nbsp; <input type="radio"> Patient centric'
+        clinAppsMsg.innerHTML=' &nbsp; <input type="button" value="<"> &nbsp; <input type="radio"> Operations &nbsp; <input type="radio"> Research &nbsp; <input type="radio"> Patient &nbsp; <input type="button" value="<">'
     }
 }
 
