fe88d8dff05ca28f7724eb3445d3c432ed46468f
Jonas Almeida <jalmeida@mathbiol.org>
2015-09-14 11:26:54 -0400

    setMsg

--- clinApps.js
+++ clinApps.js
@@ -2,5 +2,16 @@ console.log('clinApps.js loaded')
 
 clinApps= function(){
     // ini
-
+    clinApps.setMsg()
 }
+
+clinApps.setMsg=function(){
+    if(typeof(clinAppsMsg)==="object"){
+        clinAppsMsg.innerHTML=' &nbsp; &nbsp; &nbsp; &nbsp; <input type="radio"> Operations &nbsp; <input type="radio"> Research &nbsp; <input type="radio"> Patient centric'
+    }
+}
+
+// ini
+$( document ).ready(function() {
+    clinApps()
+});
