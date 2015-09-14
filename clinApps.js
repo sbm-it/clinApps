5b84b9024aaf48bcda7af1a97e58bfa9654962c2
Jonas Almeida <jalmeida@mathbiol.org>
2015-09-14 13:03:10 -0400

    transparent arrows

--- clinApps.js
+++ clinApps.js
@@ -7,7 +7,7 @@ clinApps= function(){
 
 clinApps.setMsg=function(){
     if(typeof(clinAppsMsg)==="object"){
-        clinAppsMsg.innerHTML=' &nbsp; <input type="button" value="<"> &nbsp; <input type="radio"> Operations &nbsp; <input type="radio"> Research &nbsp; <input type="radio"> Patient &nbsp; <input type="button" value="<">'
+        clinAppsMsg.innerHTML=' &nbsp; <input id="arrowType" type="button" value="<" style="background:rgba(0,0,0,0);border-width:0"> &nbsp; <input id="radioOperations" type="radio"><span id="spanOperations"> Operations &nbsp;</span> <input id="radioResearch" type="radio"><span id="spanResearch"> Research &nbsp;</span> <input id="radioPatient" type="radio"><span id="spanPatient"> Patient &nbsp;</span> <input id="arrowLogin" type="button" value="<" style="background:rgba(0,0,0,0);border-width:0">'
     }
 }
 
