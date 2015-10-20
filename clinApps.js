8bea96c8b6b77060f96f27244ab9c181ec3adffc
Jonas Almeida <jalmeida@mathbiol.org>
2015-10-20 10:21:40 -0400

    calling sum jsonp for the list of docs

--- clinApps.js
+++ clinApps.js
@@ -2,60 +2,109 @@ console.log('clinApps.js loaded')
 
 clinApps= function(){
     // ini
-    clinApps.setMsg()
-    clinApps.hashJobs()
+    clinApps.loadApps()
+    clinApps.msg('loading apps ...')
+    clinApps.getHash()
 }
 
-clinApps.setMsg=function(){
-    if(typeof(clinAppsMsg)==="object"){
-        clinAppsMsg.innerHTML='<input id="showMsgType" type="button" value="-" style="background:rgba(0,0,0,0);border-width:0"> &nbsp; <input id="radioOperations" type="radio"><span id="spanOperations"> Operations &nbsp;</span> <input id="radioResearch" type="radio"><span id="spanResearch"> Research &nbsp;</span> <input id="radioDSRIP" type="radio"><span id="spanDSRIP"> DSRIP &nbsp;</span> <input id="radioPatient" type="radio"><span id="spanPatient"> Patient &nbsp;</span> <span id="msgSpan" hidden="true"> loading ...</span>'
-        // wiring events
-        showMsgType.onclick=function(){
-            if(showMsgType.value=='-'){
-                showMsgType.value='+'
-                showMsgType.style.color="blue"
-                radioOperations.hidden=spanOperations.hidden=radioResearch.hidden=spanResearch.hidden=radioResearch.hidden=radioDSRIP.hidden=spanDSRIP.hidden=radioPatient.hidden=spanPatient.hidden=true
-                msgSpan.hidden=false
-            }else{
-                showMsgType.value='-'
-                showMsgType.style.color="black"
-                radioOperations.hidden=spanOperations.hidden=radioResearch.hidden=spanResearch.hidden=radioResearch.hidden=radioDSRIP.hidden=spanDSRIP.hidden=radioPatient.hidden=spanPatient.hidden=false
-                msgSpan.hidden=true
-            }
-        }
-        showMsgType.click()
-    }
+// plain message writter
+clinApps.msg=function(txt,clr){
+    if(!clr){clr='blue'}
+    clinAppsMsg.style.color=clr
+    clinAppsMsg.innerHTML='&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+txt
+    /*
+    var i=0,t=5
+    clinApps.msg.int = setInterval(function(){
+        if(i<txt.length)
+        clinAppsMsg.textContent+=txt[i]
+        i++
+    },t)
+    setTimeout(function(){
+        clearInterval(clinApps.msg.int)
+    },t*txt.length*2)
+    */
 }
 
-clinApps.hashJobs=function(suf){ // perform jobs submited within the hash, i.e. for suf='jobs'
-    if(!suf){suf='job'} // look for jobs in ./job/
-    var jobs = location.hash.match(RegExp(suf+'=[^&]+','g'))
-    if(jobs){
-        jobs.forEach(function(Ji){
-            // location.hash=location.hash.replace(Ji,'') // remove job from hash
-            var job=Ji.split('=')[1]+'.js'
-            if(true){ // this is a great place to further restrict allowed origins
-                console.log('loading job "'+job+'"...')
-                var s = document.createElement('script')
-                s.src=suf+'/'+job // note one can load from different subdiretories
-                document.head.appendChild(s)
-                /*
-                $.getScript('job/'+job)
-                 .then(
-                    function(res){
-                        //console.log('... "'+job+'" loaded:')
-                    },function(err){
-                        //console.log('... "'+job+'" NOT loaded:')
-                    }
-                 )
-                 */
-                 
-            }
+// load apps
+clinApps.loadApps=function(){
+    appSpace.innerHTML='' // clean it first
+    // load json manifest
+    $.getJSON('app/apps.json',function(x){
+        x.forEach(function(xi){
+            clinApps.assembleApp(xi)
         })
-        
+    })
+}
 
+// load jobs
+clinApps.getHash=function(){
+    if(location.hash.length>0){
+        clinApps.getScript(location.hash.slice(1))
     }
-        
+}
+
+clinApps.assembleApp=function(x){
+    //var 
+    img = document.createElement('img')
+    appSpace.appendChild(img)
+    img.src=x.icon
+    img.width=100
+    img.onclick=function(){
+        clinApps.getScript(x.onclick)
+        //$.getScript(x.onclick)
+        clinApps.msg(x.description,'green')
+    }
+    
+}
+
+clinApps.getScript=function(src){ // like $.getScript but loads it into the head
+    var s = document.createElement('script')
+    s.src=src
+    document.head.appendChild(s)
+}
+
+clinApps.getSbmDoctors=function(fun){ // get SBM doctors
+    if(!fun){fun=function(){console.log(clinApps.getSbmDoctors.docs)}}
+    localforage.getItem('SBMdocs_1').then(function(docs){
+        if(true){
+        //if(!docs){
+            var url='http://findadoc.uhmc.sunysb.edu/sbmed/jsonp.cfm?thisPage='
+            var i = 1
+            var docs=[]
+            jsonpCallback=function(x){
+                x.forEach(function(xi){
+                    if(xi.PICTURE==="NONE"){
+                        xi.PICTURE='http://www.stonybrookmedicine.edu/webfiles/physician-pics/placeholder.png'
+                    }else{
+                        xi.PICTURE='http://www.stonybrookmedicine.edu/webfiles/physician-pics/'+xi.PICTURE
+                    }                    
+                    docs.push(xi)
+                })
+                i++
+                console.log(i+': '+x.length)
+                clinApps.msg(' loading info on '+docs.length+' physicians','red')
+                if(x.length>0){
+                    getDoc(i)
+                }else{
+                    clinApps.msg(' loaded info on '+docs.length+' physicians','green')
+                    clinApps.getSbmDoctors.docs=docs
+                    localforage.setItem('SBMdocs',clinApps.getSbmDoctors.docs).then(function(){
+                        localStorage.setItem('SBMdocs',new Date())
+                        fun()
+                    })
+                }        
+            }
+            var getDoc=function(i){
+                $.getScript(url+i,function(x){
+                    4
+                })
+            }
+            getDoc(i) // starts here
+        }else{
+            clinApps.getSbmDoctors.docs=docs
+            fun()
+        }
+    })
 }
 
 // ini
@@ -63,4 +112,10 @@ $( document ).ready(function() {
     clinApps()
 });
 
-//console.log('Hi Maggie')
+/*
+Notes:
+
+opt=&ins=&loc=&lang=29&gen=&nm=&cn=&zip=&radius=
+opt=&ins=&loc=&lang=29&gen=&nm=&cn=&zip=&radius=
+
+*/
